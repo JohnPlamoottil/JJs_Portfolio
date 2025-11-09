78d4873c5a632a173a06f6f5900a2637f6cad1e7
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-11-08 23:15:50 -0600

    change png to jpeg

--- data/data.ts
+++ data/data.ts
@@ -1,4 +1,4 @@
-import Birthday from "../public/images/birthday.png";
+import Birthday from "../public/images/birthday.jpeg";
 import USA from "../public/images/USA.png";
 import Flight from "../public/images/Flight.png";
 
